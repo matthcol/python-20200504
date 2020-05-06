c97b313d25ccef87249297be9feb30c0ea92283d
Matthias Colin <matthias.colin@gmail.com>
2020-05-06 15:08:23 +0200

    module movie

---
+++ movie.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 14:47:23 2020
+
+@author: Matthias
+"""
+from datetime import date
+from functools import total_ordering
+
+@total_ordering
+class Movie:
+    
+    def __init__(self, title, year, director=None):
+        self.title = title
+        self.year = year
+        self.director = director
+      
+    # override __repr__ and __str__
+    def __repr__(self):
+        return f"{self.title} ({self.year}) by {self.director}"
+    
+    def __len__(self):
+        return len(self.title)
+    
+    # override __eq__ and __neq__
+    def __eq__(self, other):
+        if type(other) != Movie:
+            return NotImplemented
+        return (self.title, self.year) == (other.title, other.year)
+    
+    def __lt__(self, other):
+        if type(other) != Movie:
+            return NotImplemented
+        return (self.year, self.title) < (other.year, other.title)
+    
+    def age(self):
+        today = date.today()
+        return today.year - self.year
